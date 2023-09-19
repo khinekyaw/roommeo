9a0111c8f943cb5e9092687ac9a070a60f61f13a
daniel <danielrio126@gmail.com>
2023-09-19 21:48:28 +0630

    remove eslint warning

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -12,7 +12,7 @@ const Banner = () => {
       <div className='flex pt-14 pb-14 gap-[5%] lg:flex-row flex-col'>
         <div className='flex-1 lg:mb-0 mb-5'>
           <h1 className='text-3xl/normal lg:text-5xl/normal font-bold mb-4'>
-            Find a perfect property Where you'll love to live
+            Find a perfect property Where you&apos;ll love to live
           </h1>
           <p className='text-lg text-gray-500 mb-10'>
             We helps businesses customize, automate and scale up their ad
